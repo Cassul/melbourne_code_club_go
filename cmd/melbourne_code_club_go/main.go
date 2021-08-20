9edb1ac3be3225ff4f2b3516100de7ef7b93e245
Aysa Matueva <amatueva@zendesk.com>
2021-08-20 14:56:46 +1000

    refactor main function

--- cmd/melbourne_code_club_go/main.go
+++ cmd/melbourne_code_club_go/main.go
@@ -12,40 +12,11 @@ import (
 
 func main() {
 	ctx := context.Background()
-
-	db := types.Database{
-		Users:         search_stuff.LoadUsers(ctx),
-		Organizations: search_stuff.LoadOrganizations(ctx),
-		Tickets:       search_stuff.LoadTickets(ctx),
-	}
-
 	args := os.Args[1:]
-	if len(args) != 3 {
-		panic("number of arguments should equal 3")
-	}
-	dataType := args[0]
-	field_name := args[1]
-	query_value := args[2]
 
-	acceptedTypes := []string{"users", "tickets", "organizations"}
-
-	if !util.ContainsString(acceptedTypes, dataType) {
-		panic("wrong data type")
-	}
-
-	acceptedFields := types.DataTypes[dataType]
-	if !util.ContainsString(acceptedFields, field_name) {
-		panic("wrong field name")
-	}
-
-	switch dataType {
-	case "tickets":
-		fmt.Println("results - ", search(db.Tickets, query_value))
-	default:
-		panic("Somehow we got here with the wrong datatype")
-	}
-
-	fmt.Println("args - ", args)
+	validate(args)
+	db := loadData(ctx)
+	searchData(db, args)
 }
 
 func search(tickets []types.Ticket, search_val string) []types.Ticket {
@@ -56,4 +27,44 @@ func search(tickets []types.Ticket, search_val string) []types.Ticket {
 		}
 	}
 	return results
+}
+
+func validate(args []string) {
+	if len(args) != 3 {
+		panic("number of arguments should equal 3")
+	}
+	dataType := args[0]
+	fieldName := args[1]
+
+	acceptedTypes := []string{"users", "tickets", "organizations"}
+
+	if !util.ContainsString(acceptedTypes, dataType) {
+		panic("wrong data type")
+	}
+
+	acceptedFields := types.DataTypes[dataType]
+	if !util.ContainsString(acceptedFields, fieldName) {
+		panic("wrong field name")
+	}
+}
+
+func loadData(ctx context.Context) types.Database {
+	return types.Database{
+		Users:         search_stuff.LoadUsers(ctx),
+		Organizations: search_stuff.LoadOrganizations(ctx),
+		Tickets:       search_stuff.LoadTickets(ctx),
+	}
+}
+
+func searchData(db types.Database, args []string) {
+	dataType := args[0]
+	// fieldName := args[1]
+	query_value := args[2]
+
+	switch dataType {
+	case "tickets":
+		fmt.Println("results - ", search(db.Tickets, query_value))
+	default:
+		panic("Somehow we got here with the wrong datatype")
+	}
 }
